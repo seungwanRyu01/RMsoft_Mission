92ab49a2e14b4de16453e92b592a9f94f4ac83ec
seungwanRyu01 <seungwan6255@gmail.com>
2022-12-15 07:03:37 +0900

    fix : 주석 수정

--- src/app/Company/companyRoute.js
+++ src/app/Company/companyRoute.js
@@ -4,10 +4,10 @@ module.exports = function(app){
     // 2. 등록업체 조회 API
     app.get('/app/company', company.getCompany);
 
-    // test : 구매자 생성 API
+    // test : 등록업체 생성 API
     app.post('/app/company', company.postCompany);
 
-    // test : 구매자 변경 API
+    // test : 등록업체 변경 API
     app.patch('/app/company/:companyIdx', company.updateCompany);
 
 };
